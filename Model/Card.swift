1014ea2686a764c9e372ec13b8263c2bbff4f9cb
Tbone288 <todo3858@740s-pl-1176.local>
2016-10-25 13:15:36 -0600

    Added card classes to XCode
    Organized groups also

---
+++ Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Odom, Tyler on 10/25/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import Foundation
---
+++ Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Odom, Tyler on 10/25/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import Foundation
