77801337e40d506f08a0bae4585aaefc9fea7166
Tbone288 <todo3858@740s-pl-1176.local>
2016-10-27 12:31:27 -0600

    Changed groups and deleted files.
    Added a CardGameController to replace ViewController.

--- Model/PlayingCard.swift
+++ Model/PlayingCard.swift
@@ -10,21 +10,63 @@ import UIKit
 
 class PlayingCard : Card
 {
-    private var rank : Int
+    //private var rank : Int
     private var suit : String
+    {
+        get
+        {
+                    return self.suit
+        }
+        set(suit)
+        {
+            self.suit = suit
+        }
+    }
     private var color : UIColor
+    {
+        get
+        {
+            return self.color
+        }
+        set(color)
+        {
+            self.color = color
+        }
+    }
     
+    private var rank : Int
+    {
+        get
+        {
+            return self.rank
+        }
+        set(rank)
+        {
+            self.rank = rank
+        }
+    }
     override init()
     {
-        rank = 0
-        suit = ""
-        color = UIColor()
         super.init()
+        rank = 0
+        suit = String()
+        color = UIColor()
     }
     
     override func toString() -> String
     {
-        let description = "The playing card rank is: \(rank) and its suit is \(suit). It has a color of \(color)"
+        let backStatus : String
+        if super.isFacing()
+        {
+            backStatus = " is face up"
+        }
+        else
+        {
+            backStatus = " is face down"
+        }
+        
+        
+        let description = "The playing card rank is: \(rank) and its suit is \(suit). It has a color of \(color) and \(backStatus)."
         
         return description
     }
